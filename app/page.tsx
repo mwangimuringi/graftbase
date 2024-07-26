1bb19810bfe1031340784609153bbff735122f2a
mwangimuringi <mwangimuringi10@gmail.com>
2024-07-26 20:16:34 +0300

    initialized the starter file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-const Home = () => {
+const Page = () => {
     return (
         <div className="bg-yellow-400">
             <h1 className="bg-slate-600">Home</h1>
         </div>
     );
 }
-export default Home;
+export default Page;

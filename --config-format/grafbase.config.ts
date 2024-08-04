904654343d788bd401545decbcb1161a97628ea4
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-04 12:16:16 +0300

    ....

--- --config-format/grafbase.config.ts
+++ --config-format/grafbase.config.ts
@@ -5,15 +5,15 @@ const type  = {
   relation: Array<string>,
 };
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 20 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(),
-  projects: g.relation(() => Project).list().optional(),
-})
+// const User = g.model('User', {
+//   name: g.string().length({ min: 2, max: 20 }),
+//   email: g.string().unique(),
+//   avatarUrl: g.url(),
+//   description: g.string().optional(),
+//   githubUrl: g.url().optional(),
+//   linkedinUrl: g.url().optional(),
+//   projects: g.relation(() => Project).list().optional(),
+// })
 
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),

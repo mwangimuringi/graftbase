521693be08c41ae7a232cfb214b89ac16684f153
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-19 23:16:15 +0300

    Created the basic structure of the Navbar component.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,25 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { NavLinks } from '@/constants'
-import AuthProviders from './AuthProviders'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { NavLinks } from '@/constants';
 
 const Navbar = () => {
-  const session = null;
   return (
-    <nav className='flexBetween navbar'>
-      <div className="flex-1 justify-start gap-10 ">
+    <nav className="flexBetween navbar">
+      <div className="flex-1 justify-start gap-10">
         <Link href="./">
-          <Image
-            src="/logo.svg"
-            width={115}
-            height={41}
-            alt='Flexible'
-          />
+          <Image src="/logo.svg" width={115} height={41} alt="Flexible" />
         </Link>
-        <ul className='xl:flex hidden text-sm gap-7'>
+        <ul className="xl:flex hidden text-sm gap-7">
           {NavLinks.map((link) => (
-              <Link href={link.href} key={link.key} >{link.text}
-              </Link>
-          ))
-          }
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
         </ul>
       </div>
-
-      <div className="flexCenter gap-4">
-          {session ? (
-            <>
-              UserPhoto
-              <Link href = "/create-project">
-              </Link>
-            </>
-          ) : (
-            <AuthProviders />
-          )}
-      </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

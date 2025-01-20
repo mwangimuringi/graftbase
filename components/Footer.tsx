86a7a63b24b20ed93118d00c1c639a978a75c233
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-20 23:57:08 +0300

    ...

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,12 +23,8 @@ const FooterColumn = ({ title, links }: ColumnProps) => (
 );
 
 const Footer = () => (
-    <section className="flexStart footer flex-wrap">
-    <div className="w-full sm:w-auto flex flex-col gap-12">
-        <!-- Content remains the same -->
-    </div>
-</section>
-
+  <section className="flexStart footer">
+    <div className="flex flex-col gap-12 w-full">
       <div className="flex items-start flex-col">
         <Image
           src="/logo-purple.svg"

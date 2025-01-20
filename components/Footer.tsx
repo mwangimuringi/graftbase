28fda01a910cd79404e07514368567021285208f
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-20 23:59:23 +0300

    Prepared for localization by externalizing static text.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,89 +3,60 @@ import Image from "next/image";
 import Link from "next/link";
 
 type ColumnProps = {
-  title: string;
-  links: Array<string>;
+    title: string;
+    links: Array<string>;
 };
 
 const FooterColumn = ({ title, links }: ColumnProps) => (
-  <div className="footer_column">
-    <ul className="footer-links">
-    {links.map((link) => (
-        <Link href="/" key={link}>{link}</Link>
-    ))}
-</ul>
-    <ul className="flex flex-col gap-2 font-normal">
-    {links.map((link, index) => (
-        <Link href="/" key={`${link}-${index}`}>{link}</Link>
-    ))}
-</ul>
-  </div>
+    <div className="footer_column">
+        <h4 className="font-semibold">{title}</h4>
+        <ul className="footer-links">
+            {links.map((link, index) => (
+                <li key={`${link}-${index}`}>
+                    <Link href="/" rel="noopener noreferrer">
+                        <a>{link}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </div>
 );
 
-const Footer = () => (
-  <section className="flexStart footer">
-    <div className="flex flex-col gap-12 w-full">
-      <div className="flex items-start flex-col">
-        <Image
-          src="/logo-purple.svg"
-          width={116}
-          height={38}
-          alt="Flexibble logo in purple"
-        />
+const Footer = () => {
+    const description =
+        "Flexibble is the world's leading community for creatives to share, grow, and get hired.";
+    const copyright = "@ 2023 Flexibble. All rights reserved";
+    const projectsSubmitted = "projects submitted";
 
-        <p className="text-start text-sm font-normal mt-5 max-w-xs">
-          Flexibble is the world&apos;s leading community for creatives to
-          share, grow, and get hired.
-        </p>
-      </div>
-      <div className="flex flex-wrap gap-12">
-        <FooterColumn
-          title={footerLinks[0].title}
-          links={footerLinks[0].links}
-        />
-
-        <div className="flex-1 flex flex-col gap-4">
-          <FooterColumn
-            title={footerLinks[1].title}
-            links={footerLinks[1].links}
-          />
-          <FooterColumn
-            title={footerLinks[2].title}
-            links={footerLinks[2].links}
-          />
-        </div>
-
-        <FooterColumn
-          title={footerLinks[3].title}
-          links={footerLinks[3].links}
-        />
-
-        <div className="flex-1 flex flex-col gap-4">
-          <FooterColumn
-            title={footerLinks[4].title}
-            links={footerLinks[4].links}
-          />
-          <FooterColumn
-            title={footerLinks[5].title}
-            links={footerLinks[5].links}
-          />
-        </div>
-
-        <FooterColumn
-          title={footerLinks[6].title}
-          links={footerLinks[6].links}
-        />
-      </div>
-    </div>
-
-    <div className="flexBetween footer_copyright">
-      <p>@ 2023 Flexibble. All rights reserved</p>
-      <p className="text-gray">
-        <span className="text-black font-semibold">10,214</span> projects
-        submitted
-      </p>
-    </div>
-  </section>
-);
+    return (
+        <section className="flexStart footer flex-wrap">
+            <div className="flex flex-col gap-12 w-full">
+                <div className="flex items-start flex-col">
+                    <Image
+                        src="/logo-purple.svg"
+                        width={116}
+                        height={38}
+                        alt="Flexibble logo in purple"
+                    />
+                    <p className="text-start text-sm font-normal mt-5 max-w-xs">
+                        {description}
+                    </p>
+                </div>
+                <div className="flex flex-wrap gap-12">
+                    {footerLinks.map(({ title, links }, index) => (
+                        <FooterColumn key={index} title={title} links={links} />
+                    ))}
+                </div>
+            </div>
+            <div className="flexBetween footer_copyright">
+                <p>{copyright}</p>
+                <p className="text-gray">
+                    <span className="text-black font-semibold">10,214</span>{" "}
+                    {projectsSubmitted}
+                </p>
+            </div>
+        </section>
+    );
+};
 
 export default Footer;

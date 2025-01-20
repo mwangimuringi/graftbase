bcbb288d09af096f3f5aa3c77c9b9f9c26409f53
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-20 23:54:53 +0300

    Fix potential duplicate key issue by using unique identifiers for mapped elements.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,15 +11,10 @@ const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
     <h4 className="font-semibold">{title}</h4>
     <ul className="flex flex-col gap-2 font-normal">
-      {links.map((link) => (
-        <li key={link}>
-          <Link href="/" key={link} rel="noopener noreferrer">
-    {link}
-</Link>
-
-        </li>
-      ))}
-    </ul>
+    {links.map((link, index) => (
+        <Link href="/" key={`${link}-${index}`}>{link}</Link>
+    ))}
+</ul>
   </div>
 );
 

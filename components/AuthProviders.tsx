18ae511ffbf0816a3c9ab96716bb23bcdb51a03c
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-25 23:57:09 +0300

    Updated home component

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AuthProviders = () => {
   return (
     <div>
-      
+  This is the AuthProviders component. It will be used to display the different auth providers.
     </div>
   )
 }

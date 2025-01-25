ee67d0ff8dca265a4bc8c5ec06105f88217fdb18
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-25 23:57:35 +0300

    Styled the components

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthProviders = () => {
   return (
-    <div>
+    <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
     </div>
   )

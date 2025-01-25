edd5edb8ff16ecac9fac62945de0650bd6afd615
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-25 23:58:48 +0300

    Updated the code

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -4,6 +4,7 @@ const AuthProviders = () => {
   return (
     <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
+  <h1>AuthProviders</h1>
     </div>
   )
 }

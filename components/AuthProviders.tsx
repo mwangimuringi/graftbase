a72e50eb9b79d073c11fd46f9e017d2396004e7b
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-25 23:58:23 +0300

    ...

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -5,8 +5,6 @@ const AuthProviders = () => {
     <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
     </div>
-
-    
   )
 }
 

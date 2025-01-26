cdfae2b401b55dab0069ef573a385bac88be8ad7
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-26 23:46:50 +0300

    Removed unused elements

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -4,10 +4,6 @@ const AuthProviders = () => {
   return (
     <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
-  <h1>AuthProviders</h1>
-  <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
-  <h2>AuthProviders</h2>
-  <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
     </div>
   )
 }

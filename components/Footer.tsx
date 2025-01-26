3fe70c448bc56325ffd4ec6e62f923acef15154a
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-26 23:51:10 +0300

    Replaced unused declaration

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -55,6 +55,26 @@ const Footer = () => {
                     {projectsSubmitted}
                 </p>
             </div>
+            <div className="footer-socials">
+                <a href="https://www.linkedin.com/company/flexible-inc/" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src="/icons/linkedin.svg"
+                        width={24}
+                        height={24}
+                        alt="LinkedIn logo"
+                    />
+                </a>
+                <a href="https://twitter.com/flexible_inc" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src="/icons/twitter.svg"
+                        width={24}
+                        height={24}
+                        alt="Twitter logo"
+                    />
+                </a>
+                <a href="https://github.com/flexible-inc" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src="/icons/github.svg"
         </section>
     );
 };

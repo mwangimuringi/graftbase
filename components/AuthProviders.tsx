f89b1a220a7c5a5355f44e91d783643d29d70695
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-26 23:49:32 +0300

    implemented error handling

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -5,6 +5,8 @@ const AuthProviders = () => {
     <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
   <h3>AuthProviders</h3>
+
+  <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
     </div>
   )
 }

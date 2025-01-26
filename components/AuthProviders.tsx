016e8581fa45d8ac36a452333a3778fc952bdf7b
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-26 23:48:20 +0300

    Created declaration in auth-providers

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -4,6 +4,7 @@ const AuthProviders = () => {
   return (
     <div className="auth-providers">
   This is the AuthProviders component. It will be used to display the different auth providers.
+  <h3>AuthProviders</h3>
     </div>
   )
 }

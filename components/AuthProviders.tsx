213a825761397850389df07979aeba199a47e523
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-26 23:44:48 +0300

    added component

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -7,6 +7,8 @@ const AuthProviders = () => {
   <h1>AuthProviders</h1>
   <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
     </div>
+    <h2>AuthProviders</h2>
+    <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
   )
 }
 

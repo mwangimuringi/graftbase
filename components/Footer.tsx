d3f2ef648c2067b31e48d43f900ee957c704f689
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-27 23:37:33 +0300

    Add Footer component with reusable column structure.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,3 @@
-import { footerLinks } from "@/constants";
-import Image from "next/image";
-import Link from "next/link";
-
 type ColumnProps = {
     title: string;
     links: Array<string>;
@@ -13,90 +9,11 @@ const FooterColumn = ({ title, links }: ColumnProps) => (
         <ul className="footer-links">
             {links.map((link, index) => (
                 <li key={`${link}-${index}`}>
-                    <Link href="/" rel="noopener noreferrer">
-                        <a>{link}</a>
-                    </Link>
+                    <a href="/" rel="noopener noreferrer">{link}</a>
                 </li>
             ))}
         </ul>
     </div>
 );
 
-const Footer = () => {
-    const description =
-        "Flexibble is the world's leading community for creatives to share, grow, and get hired.";
-    const copyright = "@ 2023 Flexibble. All rights reserved";
-    const projectsSubmitted = "projects submitted";
-
-    return (
-        <section className="flexStart footer flex-wrap">
-            <div className="flex flex-col gap-12 w-full">
-                <div className="flex items-start flex-col">
-                    <Image
-                        src="/logo-purple.svg"
-                        width={116}
-                        height={38}
-                        alt="Flexibble logo in purple"
-                    />
-                    <p className="text-start text-sm font-normal mt-5 max-w-xs">
-                        {description}
-                    </p>
-                </div>
-                <div className="flex flex-wrap gap-12">
-                    {footerLinks.map(({ title, links }, index) => (
-                        <FooterColumn key={index} title={title} links={links} />
-                    ))}
-                </div>
-            </div>
-            <div className="flexBetween footer_copyright">
-                <p>{copyright}</p>
-                <p className="text-gray">
-                    <span className="text-black font-semibold">10,214</span>{" "}
-                    {projectsSubmitted}
-                </p>
-            </div>
-            <div className="footer-socials">
-                <a href="https://www.linkedin.com/company/flexible-inc/" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="/icons/linkedin.svg"
-                        width={24}
-                        height={24}
-                        alt="LinkedIn logo"
-                    />
-                </a>
-                <a href="https://twitter.com/flexible_inc" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="/icons/twitter.svg"
-                        width={24}
-                        height={24}
-                        alt="Twitter logo"
-                    />
-                </a>
-                <a href="https://github.com/flexible-inc" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="/icons/github.svg"
-                        width={24}
-                        height={24}
-                        alt="GitHub logo"
-                    />
-                    </a>
-                <a href="https://www.instagram.com/flexible_inc/" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="/icons/instagram.svg"
-                        width={24}
-                        height={24}
-                        alt="Instagram logo"
-                    />
-                </a>
-            </div>
-            <div className="footer-copyright">
-                <p>{copyright}</p>     
-                <p className="text-gray">
-                    <span className="text-black font-semibold">10,214</span> {projectsSubmitted}
-                </p>
-            </div>
-        </section>
-    );
-};
-
-export default Footer;
+export default FooterColumn;

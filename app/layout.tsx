5bd1932dab2c7fccae51373061b0c7d3120cb4b9
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-14 19:55:40 +0300

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,23 @@
-import Navbar from '@/components/Navbar';
-import './globals.css';
-import Footer from '@/components/Footer';
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import Footer from "@/components/Footer";
 export const metadata = {
-  title: 'Flexibble',
-  description: 'Showcase and discover remarkable developer projects',
-}
+  title: "Flexibble",
+  description: "Showcase and discover remarkable developer projects",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Navbar/> 
-        <main>
-          {children}
-        </main>
-        <Footer/>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
-  )
-
-  
+  );
 }

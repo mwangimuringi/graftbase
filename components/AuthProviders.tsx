c10783da402c47c35f0902612f30813a0b7c03f6
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-14 18:12:26 +0300

    ...

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const AuthProviders = () => {
   return (
     <div className="auth-providers">
-  This is the AuthProviders component. It will be used to display the different auth providers.
-  <h3>AuthProviders</h3>
-
-  <p>This is the AuthProviders component. It will be used to display the different auth providers.</p>
+      This is the AuthProviders component. It will be used to display the
+      different auth providers.
+      <h3>AuthProviders</h3>
+      <p>
+        This is the AuthProviders component. It will be used to display the
+        different auth providers.
+      </p>
     </div>
-  )
+  );
+};
 
-  
-}
-
-export default AuthProviders
+export default AuthProviders;

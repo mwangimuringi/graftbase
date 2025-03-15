9bad2124aff55aa4bfbf21002a952f016302613d
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-15 23:46:41 +0300

    added basic Button component.

---
+++ components/ui/Button.tsx
@@ -0,0 +1,20 @@
+// /components/ui/Button.tsx
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded-md"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;

c68162c42949be4033866b6830f2141bed50409b
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-15 23:56:27 +0300

    add basic Input component

---
+++ components/ui/Input.tsx
@@ -0,0 +1,23 @@
+// /components/ui/Input.tsx
+import React from "react";
+
+interface InputProps {
+  type?: string;
+  placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({ type = "text", placeholder, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+};
+
+export default Input;

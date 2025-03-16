53d3a34c1197b2718d703d729dddf5c90c3a4985
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-16 23:15:14 +0300

    Created basic Sidebar structure.

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="w-64 h-screen bg-gray-800 text-white p-4">
+      <h2 className="text-lg font-semibold">Sidebar</h2>
+    </aside>
+  );
+};
+
+export default Sidebar;

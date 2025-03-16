8b898d29bd29e91116be2f990e7f712267f02a92
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-16 22:59:26 +0300

    Implemented navigation links to Header.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
       <h1 className="text-xl font-semibold">My App</h1>
+      <nav>
+        <ul className="flex gap-4">
+          <li>
+            <Link href="/" className="hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };

3b641f83f4e3b65e61a03dc500c5e287d87197e9
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-17 23:01:55 +0300

    Added navigation links to Navbar.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
       <h1 className="text-lg font-semibold">My App</h1>
+      <div className="flex space-x-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+        <Link href="/contact" className="hover:underline">
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };

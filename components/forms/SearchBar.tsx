5d35f7c1f3a1da68fe1864f9a0e39747028bdeeb
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-17 23:29:24 +0300

    Added local state for search query.

--- components/forms/SearchBar.tsx
+++ components/forms/SearchBar.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar: React.FC = () => {
+  const [query, setQuery] = useState("");
+
   return (
     <input
       type="text"
       placeholder="Search..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
       className="border p-2 w-full rounded"
     />
   );

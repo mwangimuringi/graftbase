a052cbc512843fcc7f4c32f7eabdbe6e6919edd9
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-17 23:01:07 +0300

    Created basic Navbar structure.

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <h1 className="text-lg font-semibold">My App</h1>
+    </nav>
+  );
+};
+
+export default Navbar;

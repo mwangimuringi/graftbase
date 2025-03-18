cba0230e1afb432bc08e3c4cd4058c0ec597091e
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-18 22:45:39 +0300

    Implemented support for optional icons.

--- components/dashboard/StatsCard.tsx
+++ components/dashboard/StatsCard.tsx
@@ -1,11 +1,12 @@
-// /components/dashboard/StatsCard.tsx
 import React from "react";
+import { IconType } from "react-icons";
 
 interface StatsCardProps {
   title: string;
   value: string | number;
   bgColor?: string;
   textColor?: string;
+  Icon?: IconType;
 }
 
 const StatsCard: React.FC<StatsCardProps> = ({
@@ -13,11 +14,15 @@ const StatsCard: React.FC<StatsCardProps> = ({
   value,
   bgColor = "bg-white",
   textColor = "text-gray-800",
+  Icon,
 }) => {
   return (
-    <div className={`${bgColor} shadow-md rounded-lg p-4`}>
-      <h3 className="text-gray-600 text-sm">{title}</h3>
-      <p className={`text-2xl font-bold ${textColor}`}>{value}</p>
+    <div className={`${bgColor} shadow-md rounded-lg p-4 flex items-center`}>
+      {Icon && <Icon className="text-3xl mr-3" />}
+      <div>
+        <h3 className="text-gray-600 text-sm">{title}</h3>
+        <p className={`text-2xl font-bold ${textColor}`}>{value}</p>
+      </div>
     </div>
   );
 };

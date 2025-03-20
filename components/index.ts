5179bad288bd80a8f7b9a0b4c7b9b09f9ca8cf3c
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-20 22:48:15 +0300

    Enhanced readability with structure comments

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,12 @@
-// index.ts - Initial commit
-export {};
-
+// UI Components
 export { default as Button } from "./ui/Button";
 export { default as Modal } from "./ui/Modal";
 export { default as Input } from "./ui/Input";
 export { default as Card } from "./ui/Card";
-export { default as Table } from "./ui/Table";
-export { default as UserTable } from "./dashboard/UserTable";
-export { default as Dashboard } from "./dashboard/Dashboard";
-export { default as Logo } from "./icons/Logo";
+// export { default as Table } from "./ui/Table";
+// export { default as Dropdown } from "./ui/Dropdown";
+// export { default as Navbar } from "./ui/Navbar";
+// export { default as Footer } from "./ui/Footer";
+// export { default as Sidebar } from "./ui/Sidebar";
+// export { default as Logo } from "./ui/Logo";
+// export { default as UserIcon } from "./ui/UserIcon";

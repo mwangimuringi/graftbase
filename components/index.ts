3582bedc6a36f079517a26cfc3f0db549c4f39a8
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-20 22:46:47 +0300

    Exported core UI components for cleaner imports.

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,6 @@
 // index.ts - Initial commit
 export {};
+
+export { default as Button } from "./ui/Button";
+export { default as Modal } from "./ui/Modal";
+export { default as Input } from "./ui/Input";

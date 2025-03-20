aaf61893d916d55fd3cd4eff1eadaa6f946d3d79
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-20 22:47:14 +0300

    Extend barrel file with Input and Card components

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,8 @@ export {};
 export { default as Button } from "./ui/Button";
 export { default as Modal } from "./ui/Modal";
 export { default as Input } from "./ui/Input";
+export { default as Card } from "./ui/Card";
+export { default as Table } from "./ui/Table";
+export { default as UserTable } from "./dashboard/UserTable";
+export { default as Dashboard } from "./dashboard/Dashboard";
+export { default as Logo } from "./icons/Logo";

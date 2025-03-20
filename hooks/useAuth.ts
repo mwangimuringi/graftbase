40aa7c7b86f7bf85705dd1761d368948e235dece
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-20 22:54:48 +0300

    Added login and logout functions to manage auth state

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -10,7 +10,17 @@ const useAuth = () => {
     }
   }, []);
 
-  return { user };
+  const login = (userData: object) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return { user, login, logout };
 };
 
 export default useAuth;

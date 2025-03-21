19ac953a3276618112248826175e4f2aecb17eff
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-21 21:25:03 +0300

    Added support for custom fetch options

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (url: string) => {
+const useFetch = (url: string, options?: RequestInit) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
@@ -12,15 +12,16 @@ const useFetch = (url: string) => {
     setError(null);
     setData(null);
 
-    fetch(url)
+    fetch(url, options)
       .then((response) => {
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
         return response.json();
       })
       .then(setData)
       .catch(setError)
       .finally(() => setLoading(false));
-  }, [url]);
+  }, [url, options]);
 
   return { data, loading, error };
 };

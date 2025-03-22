4ac6ad1609d10dc3bd2874f7aaa5e0254e9f6f03
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-22 22:29:35 +0300

    Standardize exports and ensure all hooks are included

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,5 @@
+import { useAuth, useFetch, useDebounce } from "@/hooks";
+
 export { default as useAuth } from "./useAuth";
 export { default as useTheme } from "./useTheme";
 export { default as useFetch } from "./useFetch";

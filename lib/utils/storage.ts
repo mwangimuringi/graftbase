c740ee7859dec09e329f84cc80a0f35e2fb58141
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-23 23:22:32 +0300

    Added sessionStorage support to storage helpers

--- lib/utils/storage.ts
+++ lib/utils/storage.ts
@@ -1,13 +1,16 @@
 export const Storage = {
-    setItem<T>(key: string, value: T) {
-      localStorage.setItem(key, JSON.stringify(value));
+    setItem<T>(key: string, value: T, useSession = false) {
+      const storage = useSession ? sessionStorage : localStorage;
+      storage.setItem(key, JSON.stringify(value));
     },
-    getItem<T>(key: string): T | null {
-      const item = localStorage.getItem(key);
+    getItem<T>(key: string, useSession = false): T | null {
+      const storage = useSession ? sessionStorage : localStorage;
+      const item = storage.getItem(key);
       return item ? JSON.parse(item) : null;
     },
-    removeItem(key: string) {
-      localStorage.removeItem(key);
+    removeItem(key: string, useSession = false) {
+      const storage = useSession ? sessionStorage : localStorage;
+      storage.removeItem(key);
     },
   };
   

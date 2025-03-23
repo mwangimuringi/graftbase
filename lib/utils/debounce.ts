d9387e47734c86ffe9608ea329455495ab9c7789
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-23 23:13:48 +0300

    Added basic debounce function to optimize input handling.

---
+++ lib/utils/debounce.ts
@@ -0,0 +1,12 @@
+export function debounce<T extends (...args: any[]) => void>(
+    func: T,
+    delay: number
+  ) {
+    let timer: NodeJS.Timeout;
+  
+    return function (...args: Parameters<T>) {
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), delay);
+    };
+  }
+  

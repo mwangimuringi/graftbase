8905f376a239a4d7986c954cb1d9d26a4a426919
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-24 22:07:56 +0300

    Added log level filtering based on environmen

--- lib/logger.ts
+++ lib/logger.ts
@@ -3,12 +3,18 @@ export const log = (message: string) => {
 };
 
 const getTimestamp = () => new Date().toISOString();
+const LOG_LEVEL = process.env.LOG_LEVEL || "info";
 
+const levels = { error: 0, warn: 1, info: 2 };
 export const logger = {
-  info: (message: string) =>
-    console.info(`[${getTimestamp()}] [INFO] ${message}`),
-  warn: (message: string) =>
-    console.warn(`[${getTimestamp()}] [WARN] ${message}`),
+  info: (message: string) => {
+    if (levels[LOG_LEVEL] >= 2)
+      console.info(`[${getTimestamp()}] [INFO] ${message}`);
+  },
+  warn: (message: string) => {
+    if (levels[LOG_LEVEL] >= 1)
+      console.warn(`[${getTimestamp()}] [WARN] ${message}`);
+  },
   error: (message: string) =>
     console.error(`[${getTimestamp()}] [ERROR] ${message}`),
 };

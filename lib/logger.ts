dfc476f488e5216adeabf6c26c63303ba2a33104
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-24 22:00:52 +0300

    Enhanced logger with log levels.

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,3 +2,9 @@ export const log = (message: string) => {
     console.log(`[LOG] ${message}`);
   };
   
+  export const logger = {
+    info: (message: string) => console.info(`[INFO] ${message}`),
+    warn: (message: string) => console.warn(`[WARN] ${message}`),
+    error: (message: string) => console.error(`[ERROR] ${message}`),
+  };
+  

2b64f406bb09c7eb351123d11c21f445ee806247
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-26 23:56:45 +0300

    Added authentication check

--- lib/api/middleware.ts
+++ lib/api/middleware.ts
@@ -1,6 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export function middleware(req: NextRequest) {
-    console.log(`Incoming request: ${req.nextUrl.pathname}`);
-    return NextResponse.next();
-}
+export default authMiddleware({
+    publicRoutes: ["/", "/api/public"],
+    afterAuth(auth, req) {
+        if (!auth.userId && !auth.isPublicRoute) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+        return NextResponse.next();
+    },
+});

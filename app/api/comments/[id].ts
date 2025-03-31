f7417ed7763641b635928824f9c5b8ceea588997
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-31 21:32:42 +0300

    Ensured only authenticated users can fetch comments.

--- app/api/comments/[id].ts
+++ app/api/comments/[id].ts
@@ -1,12 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { authenticateUser } from "@/lib/auth";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params;
+  const authUser = await authenticateUser(req);
+  if (!authUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
+  const { id } = params;
   if (!id) {
     return NextResponse.json(
       { error: "Comment ID is required" },

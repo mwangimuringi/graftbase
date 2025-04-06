2a13a82c4a97e6c0133bcb7cd7a2acb9b6b73922
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-06 23:48:30 +0300

    Added basic styling with tailwind css

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-// /app/dashboard/page.tsx
-
 export default function DashboardPage() {
-    return (
-      <main>
-        <h1>Dashboard</h1>
-      </main>
-    );
-  }
-  
+  return (
+    <main className="p-6 bg-gray-50 min-h-screen">
+      <h1 className="text-2xl font-semibold text-gray-800">Dashboard</h1>
+    </main>
+  );
+}

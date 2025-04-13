d58ffcddea72dffab78de8d2a3aebccc7a01f5ba
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-13 23:55:45 +0300

    Add POST method to accept new product

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -6,3 +6,9 @@ export async function GET() {
     return Response.json(products)
   }
   
+  export async function POST(request: Request) {
+    const body = await request.json()
+    // In real apps, save to DB
+    return Response.json({ message: 'Product created', product: body }, { status: 201 })
+  }
+  

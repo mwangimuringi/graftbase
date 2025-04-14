323e5464438606440fdb9590c6e16325fe83b728
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-14 23:50:15 +0300

    add DELETE handler to remove a product by ID

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -3,6 +3,17 @@ import { Product } from 'graft/server/entities';
 
 const { Product } = require('graft/server/entities');
 
+const { Product } = require('graft/server/entities');
+const Product = require('graft/server/entities/Product');
+
+export default defineRoute(async (req, res) => {
+  const product = await Product.findOne(req.params.id);
+  if (!product) {
+    res.status(404).json({ error: 'Product not found' });
+    return;
+  }
+  res.json(product);
+});
 
 export default defineRoute(async (req, res) => {
   const product = await Product.findOne(req.params.id);

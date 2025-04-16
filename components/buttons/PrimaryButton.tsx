11a485c6c9cd40eaa4659b01a6f6bd83d82296af
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-16 23:47:03 +0300

    Add className and type props

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -1,18 +1,7 @@
-// components/buttons/PrimaryButton.tsx
-
-import React from 'react';
-
 type PrimaryButtonProps = {
-  children: React.ReactNode;
-  onClick?: () => void;
-};
-
-const PrimaryButton: React.FC<PrimaryButtonProps> = ({ children, onClick }) => {
-  return (
-    <button onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-export default PrimaryButton;
+    children: React.ReactNode;
+    onClick?: () => void;
+    className?: string;
+    type?: 'button' | 'submit' | 'reset';
+  };
+  

f67fa3fa925f7623cccb46bc04a404436fd7b573
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-16 23:39:25 +0300

    Add accessibility via aria-label

--- components/buttons/IconButton.tsx
+++ components/buttons/IconButton.tsx
@@ -1,23 +1,22 @@
 // components/buttons/IconButton.tsx
 
-import React from 'react';
-
+import React from "react";
 
 type IconButtonProps = {
-    icon: React.ReactNode;
-    onClick?: () => void;
-    className?: string;
-  };
-  
+  icon: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+};
 
 const IconButton: React.FC<IconButtonProps> = ({ icon, onClick }) => {
   return (
     <button
-  onClick={onClick}
-  className={`p-2 rounded-full hover:bg-gray-200 transition ${className}`}
->
-  {icon}
-</button>
+      onClick={onClick}
+      className={`p-2 rounded-full hover:bg-gray-200 transition ${className}`}
+      aria-label={ariaLabel}
+    >
+      {icon}
+    </button>
   );
 };
 

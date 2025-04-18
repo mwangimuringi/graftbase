ca9bb44684c8f356f76e97f6bcc309e65de22df9
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-18 23:42:58 +0300

    Make ToggleButton functional with toggle state

--- components/buttons/ToggleButton.tsx
+++ components/buttons/ToggleButton.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const ToggleButton = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleClick = () => setIsOn(prev => !prev);
+
   return (
-    <button>
-      Toggle
+    <button onClick={handleClick}>
+      {isOn ? 'On' : 'Off'}
     </button>
   );
 };

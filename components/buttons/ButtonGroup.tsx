47f63717954420da79c1987ad369737df00ca71b
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-18 23:53:13 +0300

    Add onClick callback support for buttons

--- components/buttons/ButtonGroup.tsx
+++ components/buttons/ButtonGroup.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 interface ButtonGroupProps {
   labels: string[];
+  onClick?: (label: string, index: number) => void;
 }
 
-export const ButtonGroup: React.FC<ButtonGroupProps> = ({ labels }) => {
+export const ButtonGroup: React.FC<ButtonGroupProps> = ({ labels, onClick }) => {
   return (
     <div className="flex gap-2">
       {labels.map((label, idx) => (
-        <button key={idx}>{label}</button>
+        <button key={idx} onClick={() => onClick?.(label, idx)}>
+          {label}
+        </button>
       ))}
     </div>
   );

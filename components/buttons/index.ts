ca63d80c27f07336017e94e64b53e5ef0d9e84fd
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 00:00:32 +0300

    Use grouped exports for cleaner structure

--- components/buttons/index.ts
+++ components/buttons/index.ts
@@ -1,7 +1,6 @@
-// Components
-export { ButtonGroup } from './ButtonGroup';
-export { SecondaryButton } from './SecondaryButton';
-export { ToggleButton } from './ToggleButton';
-
-// Utils (coming soon)
-// export { formatLabel } from '../utils/formatLabel';
+export {
+    ButtonGroup,
+    SecondaryButton,
+    ToggleButton,
+  } from './components';
+  

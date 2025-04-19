fc45be187b445b6a699cc2047ed160534f91141b
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-19 23:57:50 +0300

    Remove comment clutter, keep clean code style

--- components/buttons/index.ts
+++ components/buttons/index.ts
@@ -1,7 +1,3 @@
-/**
- * Centralized component exports for easy imports across the app.
- * TODO: Add form elements and layout components.
- */
-export * from './button';
-export * from './button-group'; // TODO: Add more button group components
-export * from './button-link';
+export { ButtonGroup } from './ButtonGroup';
+export { SecondaryButton } from './SecondaryButton';
+export { ToggleButton } from './ToggleButton';

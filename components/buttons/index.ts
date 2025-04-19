357192768238666485d3e0aca4a89b55acb073e7
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-19 23:59:19 +0300

    Ensure no duplicate exports when using wildcard export

--- components/buttons/index.ts
+++ components/buttons/index.ts
@@ -1,4 +1,4 @@
+// Explicit re-exports for tree shaking and typing
 export { ButtonGroup } from './ButtonGroup';
 export { SecondaryButton } from './SecondaryButton';
 export { ToggleButton } from './ToggleButton';
-\

ca3ee7c2fa7e35aae4e3d77acc1d31e77f742503
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-19 23:56:12 +0300

    Add description of index file usage

--- components/buttons/index.ts
+++ components/buttons/index.ts
@@ -1,7 +1,11 @@
+/**
+ * Centralized component exports for easy imports across the app.
+ */
+
 // Buttons
 export { ButtonGroup } from './ButtonGroup';
 export { SecondaryButton } from './SecondaryButton';
 export { ToggleButton } from './ToggleButton';
 
 // Inputs
-// export { InputField } from './InputField'; // coming soon
+// export { InputField } from './InputField';

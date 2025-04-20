83da76614fdde9c743a52235b052add3bf807621
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 23:58:26 +0300

    Allow passing custom className to extend styles

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -3,20 +3,26 @@ type Props = {
   onClick?: () => void;
   disabled?: boolean;
   fullWidth?: boolean;
+  className?: string;
 };
 
-const PrimaryButton = ({ label, onClick, disabled, fullWidth }: Props) => {
+const PrimaryButton = ({
+  label,
+  onClick,
+  disabled,
+  fullWidth,
+  className = '',
+}: Props) => {
   return (
     <button
-    className={`px-4 py-2 rounded text-white focus:outline-none focus:ring-2 focus:ring-blue-300 ${
-      disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-600'
-    } ${fullWidth ? 'w-full' : ''}`}
-    onClick={onClick}
-    disabled={disabled}
-    aria-disabled={disabled}
-  >
-    {label}
-  </button>
-  
+      className={`px-4 py-2 rounded text-white focus:outline-none focus:ring-2 focus:ring-blue-300 ${
+        disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-600'
+      } ${fullWidth ? 'w-full' : ''} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+      aria-disabled={disabled}
+    >
+      {label}
+    </button>
   );
 };

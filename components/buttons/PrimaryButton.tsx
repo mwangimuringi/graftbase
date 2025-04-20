5f1345044106290de4dabcb84290a43fc5b1aefb
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 23:59:09 +0300

    Added default props and better type safety

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -6,13 +6,13 @@ type Props = {
   className?: string;
 };
 
-const PrimaryButton = ({
+const PrimaryButton: React.FC<Props> = ({
   label,
-  onClick,
-  disabled,
-  fullWidth,
+  onClick = () => {},
+  disabled = false,
+  fullWidth = false,
   className = '',
-}: Props) => {
+}) => {
   return (
     <button
       className={`px-4 py-2 rounded text-white focus:outline-none focus:ring-2 focus:ring-blue-300 ${

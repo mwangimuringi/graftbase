169322d08599e4c4c764d5614f869a214dfe8a48
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 23:51:28 +0300

    Finalized and document current export list

--- components/buttons/index.ts
+++ components/buttons/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Component exports for Graftbase UI
+ * Easily import components from one file instead of deep paths
+ */
+
 export { ButtonGroup } from './ButtonGroup';
 export { SecondaryButton } from './SecondaryButton';
 export { ToggleButton } from './ToggleButton';

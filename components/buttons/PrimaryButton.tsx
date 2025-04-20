8f8874bcfd608e3d71386d26fafa676614919510
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 23:57:24 +0300

    Added fullWidth prop for responsive layout

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -2,14 +2,15 @@ type Props = {
   label: string;
   onClick?: () => void;
   disabled?: boolean;
+  fullWidth?: boolean;
 };
 
-const PrimaryButton = ({ label, onClick, disabled }: Props) => {
+const PrimaryButton = ({ label, onClick, disabled, fullWidth }: Props) => {
   return (
     <button
       className={`px-4 py-2 rounded text-white ${
         disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-600'
-      }`}
+      } ${fullWidth ? 'w-full' : ''}`}
       onClick={onClick}
       disabled={disabled}
     >

155c69f70406c16ede1fffff7b7b63e7fba3a071
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-20 23:55:43 +0300

    Add basic Tailwind styling for button

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -7,9 +7,10 @@ type Props = {
 
 const PrimaryButton = ({ label }: Props) => {
   return (
-    <button>
-      {label}
-    </button>
+    <button className="bg-blue-600 text-white px-4 py-2 rounded">
+  {label}
+</button>
+
   );
 };
 

d452a5fffcc47a3e00526d216afc06646dc8185f
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-22 23:52:56 +0300

    Animated modal with fade-in transition

--- components/Forms.tsx
+++ components/Forms.tsx
@@ -8,3 +8,4 @@ useEffect(() => {
     return () => document.removeEventListener('keydown', handleKeyDown);
   }, [onClose]);
   
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300 opacity-100">

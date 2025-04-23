5781724538e99d5932c57bcde6e73aa34e67fdce
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-23 23:56:31 +0300

    Hide tooltip by default, show on hover using Tailwind

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -9,9 +9,11 @@ interface TooltipProps {
 
 const Tooltip = ({ text, children }: TooltipProps) => {
   return (
-    <span>
+    <span className="relative group">
       {children}
-      <span>{text}</span>
+      <span className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-black text-white text-xs px-2 py-1 rounded">
+        {text}
+      </span>
     </span>
   );
 };

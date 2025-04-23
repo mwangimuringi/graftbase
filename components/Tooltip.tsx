93d683fb8d2941c5c499490fc28def2332dfd9c8
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-23 23:58:56 +0300

    Accepted optional position prop .

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -5,17 +5,26 @@ import React from 'react';
 interface TooltipProps {
   text: string;
   children: React.ReactNode;
+  position?: 'top' | 'right' | 'bottom' | 'left';
 }
 
-const Tooltip = ({ text, children }: TooltipProps) => {
+const positionStyles: Record<string, string> = {
+  top: 'bottom-full left-1/2 -translate-x-1/2 mb-2',
+  right: 'left-full top-1/2 -translate-y-1/2 ml-2',
+  bottom: 'top-full left-1/2 -translate-x-1/2 mt-2',
+  left: 'right-full top-1/2 -translate-y-1/2 mr-2',
+};
+
+const Tooltip = ({ text, children, position = 'top' }: TooltipProps) => {
   return (
     <span className="relative group inline-block">
       {children}
-      <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block z-10">
+      <div
+        className={`absolute hidden group-hover:block z-10 ${positionStyles[position]}`}
+      >
         <div className="bg-black text-white text-xs px-2 py-1 rounded">
           {text}
         </div>
-        <div className="w-2 h-2 bg-black rotate-45 mx-auto mt-[-4px]" />
       </div>
     </span>
   );

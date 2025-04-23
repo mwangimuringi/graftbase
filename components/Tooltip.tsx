e5e88d50d71fb626bb2fe33ee7898d9667065770
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-23 23:55:03 +0300

    Accepted children and text as props.

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -2,8 +2,18 @@
 
 import React from 'react';
 
-const Tooltip = () => {
-  return <span>Tooltip</span>;
+interface TooltipProps {
+  text: string;
+  children: React.ReactNode;
+}
+
+const Tooltip = ({ text, children }: TooltipProps) => {
+  return (
+    <span>
+      {children}
+      <span>{text}</span>
+    </span>
+  );
 };
 
 export default Tooltip;

4dad54ba5a10e2df854ddc49f4e66a9b5e4c1903
mwangimuringi <mwangimuringi10@gmail.com>
2025-04-29 23:50:59 +0300

    Add form structure with input fields

--- components/PaymentForm.tsx
+++ components/PaymentForm.tsx
@@ -3,7 +3,14 @@
 import React from 'react';
 
 const PaymentForm = () => {
-  return <div>Payment Form</div>;
+  return (
+    <form>
+      <h2>Payment Details</h2>
+      <input type="text" name="name" placeholder="Name on Card" />
+      <input type="text" name="cardNumber" placeholder="Card Number" />
+      <input type="text" name="expiry" placeholder="MM/YY" />
+      <input type="text" name="cvv" placeholder="CVV" />
+      <button type="submit">Pay Now</button>
+    </form>
+  );
 };
-
-export default PaymentForm;
